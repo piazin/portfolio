e6b35f5b12ff89373c7c9e6315d6c57e4074ca75
Lucas <ls4803326@gmail.com>
2022-10-26 14:58:45 -0300

    added new styles in the section about

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,25 +3,40 @@ import "./styles.css";
 
 function Main() {
   return (
-    <main id="main">
-      <div className="box-about">
-        <span>--- my name is</span>
-        <h1>Lucas Souza</h1>
+    <main id="container">
+      <div className="about-container">
+        <div className="about-box-presentation">
+          <span className="about-presentation-arrow"></span>
+          <p className="about-presentation-text">hello my name is</p>
+        </div>
+        <div className="about-box-title">
+          <h2 className="about-text__my-name">Lucas</h2>
+          <h2 className="about-text__my-name">Souza.</h2>
+        </div>
         <p>
           Creative front-end developer with more than +2 years of experience in
           enterprise companies and startups. Proficient in Html, Tailwind Css,
           Javascript and React. Passionate about UI/UX
         </p>
 
-        <div className="box-social-icons">
-          <span>🙃</span>
-          <span>🗿</span>
-          <span>🐏</span>
+        <div className="icons-container">
+          <a href="">
+            <i class="bx bxl-github bx-md bx-tada-hover"></i>
+          </a>
+          <a href="">
+            <i class="bx bxl-linkedin bx-md"></i>
+          </a>
+          <a href="">
+            <i class="bx bxl-instagram bx-md"></i>
+          </a>
+          <a href="">
+            <i class="bx bxl-dev-to bx-md"></i>
+          </a>
         </div>
       </div>
 
       <div className="box-img">
-        <span>retangle 1</span>
+        <span></span>
         <span></span>
       </div>
     </main>

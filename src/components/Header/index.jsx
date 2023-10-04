6996e05c336c0b63952cd2c08ab4f7072051bbfe
Lucas <ls4803326@gmail.com>
2023-10-03 22:45:06 -0300

    header fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Translator from '../i18n/Translator';
 import I18n from '../i18n/I18n';
 import logo from '../../assets/logo.png';
@@ -6,10 +6,26 @@ import './styles.css';
 
 function Header() {
   const toogleMenuMobile = () => {
-    document
-      .querySelector('.menu-mobile')
-      .classList.toggle('menu-mobile-active');
+    document.querySelector('.menu-mobile').classList.toggle('menu-mobile-active');
   };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
+  function onScroll() {
+    const header = document.querySelector('#header');
+
+    if (window.scrollY > 150) {
+      header.classList.add('header-active');
+    } else {
+      header.classList.remove('header-active');
+    }
+  }
 
   return (
     <header id="header">
@@ -90,12 +106,7 @@ function Header() {
         </li>
       </ul>
       <div className="btn-hambuguer-mobile">
-        <input
-          type="checkbox"
-          id="checkbox-menu"
-          hidden
-          onChange={() => toogleMenuMobile()}
-        />
+        <input type="checkbox" id="checkbox-menu" hidden onChange={() => toogleMenuMobile()} />
 
         <label htmlFor="checkbox-menu">
           <span></span>
